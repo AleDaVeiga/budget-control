ab955e1e0c922cb6b0613d0b01756bc412ec5f69
Alessandro da Veiga <alessandrodaveiga@gmail.com>
2017-05-09 22:42:00 -0300

    Criando a aplicação para controle de finanças pessoais.

---
+++ src/Models/BillReceive.php
@@ -0,0 +1,14 @@
+<?php
+namespace SONFin\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BillReceive extends Model{
+    
+    protected $fillable = [
+        'date_launch',
+        'name',
+        'value',
+        'user_id'
+    ];
+}
---
+++ src/controllers/users.php
@@ -0,0 +1,63 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface;
+use SONFin\Models\User;
+
+$app
+	->get(
+		'/users', function() use ($app){
+			$view = $app->service('view.renderer');
+			$repository = $app->service('user.repository');
+			$users = $repository->all();
+			return $view->render('users/list.html.twig', ['users' => $users]);
+		}, 'users.list'
+	)
+	->get(
+		'/users/new', function() use ($app){
+			$view = $app->service('view.renderer');
+			return $view->render('users/create.html.twig');
+		}, 'users.new'
+	)
+	->post(
+		'/users/store', function(ServerRequestInterface $request) use ($app){
+			$data = $request->getParsedBody();
+			$repository = $app->service('user.repository');
+			$repository->create($data);
+			return $app->route('users.list');
+		}, 'users.store'
+	)
+	->get(
+		'/users/{id}/edit', function(ServerRequestInterface $request) use ($app){
+			$view = $app->service('view.renderer');
+			$id = $request->getAttribute('id');
+			$repository = $app->service('user.repository');
+			$user = $repository->find($id);
+			return $view->render('users/edit.html.twig', ['user' => $user]);
+		}, 'users.edit'
+	)
+	->post(
+		'/users/{id}/update', function(ServerRequestInterface $request) use ($app){
+			$id = $request->getAttribute('id');
+			$data = $request->getParsedBody();	
+			$repository = $app->service('user.repository');		
+			$repository->update($id, $data);
+			return $app->route('users.list');
+		}, 'users.update'
+	)
+	->get(
+		'/users/{id}/show', function(ServerRequestInterface $request) use ($app){
+			$view = $app->service('view.renderer');
+			$id = $request->getAttribute('id');
+			$repository = $app->service('user.repository');
+			$user = $repository->find($id);
+			return $view->render('users/show.html.twig', ['user' => $user]);
+		}, 'users.show'
+	)
+	->get(
+		'/users/{id}/delete', function(ServerRequestInterface $request) use ($app){
+			$id = $request->getAttribute('id');
+			$repository = $app->service('user.repository');
+			$repository->delete($id);
+			return $app->route('users.list');
+		}, 'users.delete'
+	);
